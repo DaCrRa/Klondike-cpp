65fa95a5432f4d2d2d1c00ffe9d55590fa857bc0
Dani C R <danielcr86@gmail.com>
2016-05-13 22:26:36 +0100

    Testing the RandomGameActionController

--- src/KlondikeMain.cc
+++ src/KlondikeMain.cc
@@ -4,11 +4,13 @@
 #include <Klondike.h>
 #include <GameConsoleView.h>
 #include <UserGameActionController.h>
+#include <RandomGameActionController.h>
 
 int main(int argc, char* argv[]) {
 	Klondike k;
 	GameConsoleView view(&k);
-	UserGameActionController controller(&k);
+	//UserGameActionController controller(&k);
+	RandomGameActionController controller(&k);
 
 	k.initialize();
 
