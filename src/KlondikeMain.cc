cd1e577b40fafa11a24e49e9926c0061123dd7d2
Dani C R <danielcr86@gmail.com>
2016-05-14 14:12:28 +0100

    Use a menu to select demo or player game

--- src/KlondikeMain.cc
+++ src/KlondikeMain.cc
@@ -5,16 +5,31 @@
 #include <GameConsoleView.h>
 #include <UserGameActionController.h>
 #include <RandomGameActionController.h>
+#include <ItemSelectionDialog.h>
+#include <Menu.h>
 
 int main(int argc, char* argv[]) {
 	Klondike k;
 	GameConsoleView view(&k);
-	//UserGameActionController controller(&k);
-	RandomGameActionController controller(&k);
+	GameActionController* controller;
 
-	k.initialize();
+	UserGameActionController userController(&k);
+	RandomGameActionController randomController(&k);
 
-	while (true) {
-		view.interact(&controller);
+	Menu<GameActionController*> m("Options:", std::vector<std::pair<MenuOption, GameActionController*> >({
+		{ MenuOption("Start one-player klondike", 0), &userController },
+		{ MenuOption("Demo", 0), &randomController }
+	}));
+	controller = m.getUserSelection();
+
+	try {
+
+		k.initialize();
+
+		for (int i = 0; i < 20; ++i) {
+			view.interact(controller);
+		}
+	} catch (CancelledDialogException& e) {
+		std::cout << e.what() << std::endl;
 	}
 }
