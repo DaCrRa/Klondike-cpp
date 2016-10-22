df6063e73d45bf197cf5e8696c3d49af97e42657
Dani C R <danielcr86@gmail.com>
2016-10-22 19:25:41 +0100

    GameActionScoreCalculator implements GameActionVisitor

--- src/controllers/score/GameActionScoreCalculator.cc
+++ src/controllers/score/GameActionScoreCalculator.cc
@@ -3,6 +3,16 @@
 void GameActionScoreCalculator::visit(StockAction* stockAction) {
     scoreDelta = 0;
 }
+
+void GameActionScoreCalculator::visit(ForwardGameAction* fwdGameAction) {
+    fwdGameAction->accept(this);
+}
+
+
+void GameActionScoreCalculator::visit(UndoGameAction* undoGameAction) {
+
+}
+
 void GameActionScoreCalculator::visit(Move* move) {
     moveScoreCalculator = std::make_shared<MoveScoreCalculator>(move, scoreDelta);
     move->acceptOriginVisitor(moveScoreCalculator.get());
