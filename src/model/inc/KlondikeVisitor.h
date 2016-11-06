5aa77d85a7d579dd1187038394e35fed9292b896
Dani C R <danielcr86@gmail.com>
2016-11-06 05:49:44 +0000

    Added KlondikeLamdaVistor
    Allows to define the visiting functions as
    lambdas.

--- src/model/inc/KlondikeVisitor.h
+++ src/model/inc/KlondikeVisitor.h
@@ -12,6 +12,8 @@
 #include <Stock.h>
 #include <TableauPile.h>
 
+#include <functional>
+
 class KlondikeVisitor {
 public:
     virtual void visitScore(int score) {}
@@ -21,6 +23,66 @@ public:
     virtual void visitFoundation(Foundation* f) {}
     virtual void allFoundationsVisited() {};
     virtual ~KlondikeVisitor() {}
+};
+
+class KlondikeLambdaVisitor : public KlondikeVisitor {
+private:
+    std::function<void(int)> visitScoreFunction;
+    std::function<void(Stock*)> visitStockFunction;
+    std::function<void(TableauPile*)> visitTableauPileFunction;
+    std::function<void(Foundation*)> visitFoundationFunction;
+    std::function<void()> allTableauPilesVisitedFunction;
+    std::function<void()> allFoundationsVisitedFunction;
+public:
+    KlondikeLambdaVisitor() :
+        visitScoreFunction([&](...)->void{}),
+        visitStockFunction([&](...)->void{}),
+        visitTableauPileFunction([&](...)->void{}),
+        visitFoundationFunction([&](...)->void{}),
+        allTableauPilesVisitedFunction([&](...)->void{}),
+        allFoundationsVisitedFunction([&](...)->void{}) {}
+    KlondikeLambdaVisitor& setVisitScoreFunction(std::function<void(int)> f) {
+        visitScoreFunction = f;
+        return *this;
+    }
+    KlondikeLambdaVisitor& setVisitStockFunction(std::function<void(Stock*)> f) {
+        visitStockFunction = f;
+        return *this;
+    }
+    KlondikeLambdaVisitor& setVisitTableauPileFunction(std::function<void(TableauPile*)> f) {
+        visitTableauPileFunction = f;
+        return *this;
+    }
+    KlondikeLambdaVisitor& setVisitFoundationFunction(std::function<void(Foundation*)> f) {
+        visitFoundationFunction = f;
+        return *this;
+    }
+    KlondikeLambdaVisitor& setAllTableauPilesVisitedFunction(std::function<void()> f) {
+        allTableauPilesVisitedFunction = f;
+        return *this;
+    }
+    KlondikeLambdaVisitor& setAllFoundationsVisitedFunction(std::function<void()> f) {
+        allFoundationsVisitedFunction = f;
+        return *this;
+    }
+    void visitScore(int score) {
+        visitScoreFunction(score);
+    }
+    void visitStock(Stock* s) {
+        visitStockFunction(s);
+    }
+    void visitTableauPile(TableauPile* tp) {
+        visitTableauPileFunction(tp);
+    }
+    void allTableauPilesVisited() {
+        allTableauPilesVisitedFunction();
+    };
+    void visitFoundation(Foundation* f) {
+        visitFoundationFunction(f);
+    }
+    void allFoundationsVisited() {
+        allFoundationsVisitedFunction();
+    };
 };
 
 #endif /* SRC_MODEL_INC_KLONDIKEVISITOR_H_ */
