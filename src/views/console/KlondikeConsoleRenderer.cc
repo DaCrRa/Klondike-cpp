ebd3d5264a210e8b3a4ae264e719c3bdf3c30c6b
Dani C R <danielcr86@gmail.com>
2016-11-06 03:58:02 +0000

    KlondikeConsoleRenderer is a KlondikeVisitor
    Removed generic KlondikeRenderer as it is
    not needed for now (YAGNI)

--- src/views/console/KlondikeConsoleRenderer.cc
+++ src/views/console/KlondikeConsoleRenderer.cc
@@ -13,24 +13,15 @@
 
 void KlondikeConsoleRenderer::render() {
     std::cout << std::endl;
-    renderScore();
-    std::cout << std::endl;
-    renderStock();
-    std::cout << std::endl;
-    renderFoundations();
-    std::cout << std::endl;
-    renderTableau();
+    game->accept(this);
+}
+
+void KlondikeConsoleRenderer::visitScore(int score) {
+    std::cout << "Score: " << score << std::endl;
     std::cout << std::endl;
 }
 
-void KlondikeConsoleRenderer::renderScore() {
-    assert(game != nullptr);
-    std::cout << "Score: " << game->getScore() << std::endl;
-}
-
-void KlondikeConsoleRenderer::renderStock() {
-    assert(game != nullptr);
-    const Stock* stock = game->getStock();
+void KlondikeConsoleRenderer::visitStock(Stock* stock) {
     assert(stock != nullptr);
     std::cout << "Stock: ";
     if (stock->hasCoveredCards()) {
@@ -43,20 +34,12 @@ void KlondikeConsoleRenderer::renderStock() {
         cardRenderer.renderCard(stock->showAvailableCard());
     }
     std::cout << std::endl;
+    std::cout << std::endl;
 }
 
-void KlondikeConsoleRenderer::renderFoundations() {
-    assert(game != nullptr);
-    std::vector<Foundation>& foundations = game->getFoundations();
-    char foundationId = 'A';
-    for (std::vector<Foundation>::const_iterator it = foundations.begin(); it != foundations.end(); ++it) {
-        std::cout << "Foundation " << foundationId++ << ": ";
-        renderFoundation(it.base());
-    }
-}
-
-void KlondikeConsoleRenderer::renderFoundation(const Foundation* f) {
+void KlondikeConsoleRenderer::visitFoundation(Foundation* f) {
     assert(f != nullptr);
+    std::cout << "Foundation " << foundationTag++ << ": ";
     if (f->getNumCards() == 0) {
         std::cout << "[_]";
     }
@@ -72,23 +55,24 @@ void KlondikeConsoleRenderer::renderFoundation(const Foundation* f) {
     std::cout << std::endl;
 }
 
-void KlondikeConsoleRenderer::renderTableau() {
-    assert(game != nullptr);
-    std::vector<TableauPile>& tableau = game->getTableau();
-    int tableauPileId = 1;
-    for (std::vector<TableauPile>::const_iterator it = tableau.begin(); it != tableau.end(); ++it) {
-        std::cout << "Tableau Pile " << tableauPileId++ << ": ";
-        renderTableauPile(it.base());
-    }
-}
-
-void KlondikeConsoleRenderer::renderTableauPile(const TableauPile* tp) {
+void KlondikeConsoleRenderer::visitTableauPile(TableauPile* tp) {
     assert(tp != nullptr);
+
+    std::cout << "Tableau Pile " << tableauPileTag++ << ": ";
+
     for (int i = 0; i < tp->getNumCoveredCards(); ++i) {
         std::cout << "[";
     }
     for (PileIterator it = tp->uncoveredCardsBegin(); it != tp->uncoveredCardsEnd(); ++it) {
         cardRenderer.renderCard(*it);
     }
+    std::cout << std::endl;
+}
+
+void KlondikeConsoleRenderer::allFoundationsVisited() {
+    std::cout << std::endl;
+}
+
+void KlondikeConsoleRenderer::allTableauPilesVisited() {
     std::cout << std::endl;
 }
--- src/views/console/inc/KlondikeConsoleRenderer.h
+++ src/views/console/inc/KlondikeConsoleRenderer.h
@@ -8,21 +8,26 @@
 #ifndef SRC_INC_KLONDIKECONSOLERENDERER_H_
 #define SRC_INC_KLONDIKECONSOLERENDERER_H_
 
+#include <Klondike.h>
 #include <CardConsoleRenderer.h>
-#include <KlondikeRenderer.h>
 
-class KlondikeConsoleRenderer: public KlondikeRenderer {
+#include <memory>
+
+class KlondikeConsoleRenderer: public KlondikeVisitor {
 private:
     CardConsoleRenderer cardRenderer;
+    std::shared_ptr<Klondike>& game;
+    char foundationTag = 'A';
+    char tableauPileTag = '1';
 public:
-    KlondikeConsoleRenderer(std::shared_ptr<Klondike>& g) : KlondikeRenderer(g) {};
+    KlondikeConsoleRenderer(std::shared_ptr<Klondike>& g) : game(g) {};
     void render();
-    void renderScore();
-    void renderStock();
-    void renderFoundations();
-    void renderTableau();
-    void renderFoundation(const Foundation* f);
-    void renderTableauPile(const TableauPile* tp);
+    void visitScore(int score);
+    void visitStock(Stock* s);
+    void visitFoundation(Foundation* f);
+    void visitTableauPile(TableauPile* f);
+    void allFoundationsVisited();
+    void allTableauPilesVisited();
 };
 
 #endif /* SRC_INC_KLONDIKECONSOLERENDERER_H_ */
