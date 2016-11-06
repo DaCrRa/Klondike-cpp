ba064161f9b609064a7ad47fce4e071aadaae4df
Dani C R <danielcr86@gmail.com>
2016-11-06 09:47:24 +0000

    Added public static constants in KlondikeConsoleRenderer
    The idea is to reuse them when tagging game elemens
    in MoveCardView

--- src/views/console/inc/KlondikeConsoleRenderer.h
+++ src/views/console/inc/KlondikeConsoleRenderer.h
@@ -17,8 +17,8 @@ class KlondikeConsoleRenderer: public KlondikeVisitor {
 private:
     CardConsoleRenderer cardRenderer;
     std::shared_ptr<Klondike>& game;
-    char foundationTag = 'A';
-    char tableauPileTag = '1';
+    char foundationTag = FOUNDATION_BASE_TAG;
+    char tableauPileTag = TABLEAU_PILE_BASE_TAG;
 public:
     KlondikeConsoleRenderer(std::shared_ptr<Klondike>& g) : game(g) {};
     void render();
@@ -28,6 +28,8 @@ public:
     void visitTableauPile(TableauPile* f);
     void allFoundationsVisited();
     void allTableauPilesVisited();
+    static const char FOUNDATION_BASE_TAG = 'A';
+    static const char TABLEAU_PILE_BASE_TAG = '1';
 };
 
 #endif /* SRC_INC_KLONDIKECONSOLERENDERER_H_ */
