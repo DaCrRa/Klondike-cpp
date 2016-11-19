ec77c360c984e4f03d76ff93bd67c243f4514370
Dani C R <danielcr86@gmail.com>
2016-11-19 19:22:06 +0000

    Ensure TestCards always have different colors
    to test movements between tableau piles easier

--- src/cards/test_cards/TestDeck.cc
+++ src/cards/test_cards/TestDeck.cc
@@ -10,7 +10,7 @@
 TestDeck::TestDeck() {
     int numberOfCards = NUM_CARDS_PER_SUIT * NUM_SUITS;
     for (int i = 0; i < numberOfCards; ++i) {
-        cards.push_back(Card(i, i%2, 0, 0));
+        cards.push_back(Card(i, i%2, 0, i));
         remainingCardIds.push_back(i);
     }
 }
